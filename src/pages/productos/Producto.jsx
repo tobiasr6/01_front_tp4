16c54e68c3aa6da9c233df9f87fc28e1a434bb5b
tobiasr6 <requenatobias3@gmail.com>
2024-10-23 00:13:17 -0300

    change: Agregado nuevo componente.

--- src/pages/productos/Producto.jsx
+++ src/pages/productos/Producto.jsx
@@ -1,10 +1,12 @@
 import './producto.css'
-import Card from '../card/Card'
+// import Card from '../card/Card'
+import Card2 from '../card/card2/Card2'
 
 export const Producto = () => {
     return (
         <div>
-            <Card />
+            {/* <Card /> */}
+            <Card2/>
         </div>
     )
 }
